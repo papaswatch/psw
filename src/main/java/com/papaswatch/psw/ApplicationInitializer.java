29065a3b9bdaebf501343b2f5af8f2570870fa80
anseongjin <ansj@eseict.com>
2025-03-02 19:45:05 +0900

    chore: application initializer 추가 및 웹 훅 테스트

---
+++ src/main/java/com/papaswatch/psw/ApplicationInitializer.java
@@ -0,0 +1,13 @@
+package com.papaswatch.psw;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+@Slf4j
+public class ApplicationInitializer implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("Application initialized");
+    }
+}
