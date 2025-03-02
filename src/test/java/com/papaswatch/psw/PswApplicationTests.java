5812993cece154774c25d697468a790f1ff7e451
ssiky <yangss@eseict.com>
2025-03-02 16:18:55 +0900

    init

---
+++ src/test/java/com/papaswatch/psw/PswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaswatch.psw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PswApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
