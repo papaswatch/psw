4ed1829907dce550c236ed39d3a7ba7cc2ce6472
ssiky <yangss@eseict.com>
2025-03-09 17:44:40 +0900

    feat(login): 일단해보자

--- src/main/java/com/papaswatch/psw/login/testlogin.java
+++ src/main/java/com/papaswatch/psw/login/testlogin.java
@@ -2,6 +2,6 @@ package com.papaswatch.psw.login;
 
 public class testlogin {
     public static void main(String[] args) {
-        System.out.println("싫어");
+        System.out.println("싫어333");
     }
 }
