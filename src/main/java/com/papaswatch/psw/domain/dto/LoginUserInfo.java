17e6b0bc87fd88a5f9057570f708f1731209760d
ssiky <yangss@eseict.com>
2025-03-10 23:25:29 +0900

    feat(login): Login 세션 쿠키 유효기간 30분 기능 추가.

---
+++ src/main/java/com/papaswatch/psw/domain/dto/LoginUserInfo.java
@@ -0,0 +1,14 @@
+package com.papaswatch.psw.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
+public class LoginUserInfo {
+    private String loginId;
+    private String email;
+    private String phoneNumber;
+}
--- src/main/java/com/papaswatch/psw/service/UserService.java
+++ src/main/java/com/papaswatch/psw/service/UserService.java
@@ -1,8 +1,10 @@
 package com.papaswatch.psw.service;
 
 import com.papaswatch.psw.common.dto.KeyValue;
+import com.papaswatch.psw.domain.dto.LoginUserInfo;
 import com.papaswatch.psw.domain.entity.UserInfo;
 import com.papaswatch.psw.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,12 +18,23 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public boolean login(KeyValue<String, String> userInfo) {
+    public boolean login(KeyValue<String, String> userInfo, HttpSession session) {
         String userId = userInfo.getKey();
         String password = userInfo.getValue();
 
+        // 유저의 아이디와 비밀번호 정보로 유저가 존재하는지 DB에서 확인합니다.
         Optional<UserInfo> LoginRequestUserInfo = userRepository.findUserInfoByLoginIdAndPassword(userId, password);
-
-        return LoginRequestUserInfo.isPresent();
+        // 유저의 정보가 존재한다면
+        if (LoginRequestUserInfo.isPresent()) {
+            UserInfo loginRequestUserInfo = LoginRequestUserInfo.get();
+            // 세션에 담을 유저 객체를 생성합니다.
+            LoginUserInfo user = LoginUserInfo.of(loginRequestUserInfo.getLoginId(), loginRequestUserInfo.getEmail(), loginRequestUserInfo.getPhoneNumber());
+            // 세션에 유저 정보를 저장합니다.
+            session.setAttribute("USER", user);
+            // 세션 유효 시간 30분으로 설정
+            session.setMaxInactiveInterval(1800);
+            return true;
+        }
+        return false;
     }
 }
