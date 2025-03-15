b54cd042653133f65d0facfeb939e81bd0279d19
ssiky <yangss@eseict.com>
2025-03-15 13:34:15 +0900

    feat(#29): 판매자인증 api 수신부 개발

--- src/main/java/com/papaswatch/psw/controller/UserController.java
+++ src/main/java/com/papaswatch/psw/controller/UserController.java
@@ -2,14 +2,15 @@ package com.papaswatch.psw.controller;
 
 import com.papaswatch.psw.common.dto.KeyValue;
 import com.papaswatch.psw.common.dto.Response;
+import com.papaswatch.psw.common.dto.SellerValidateReq;
+import com.papaswatch.psw.common.dto.SignupReq;
 import com.papaswatch.psw.dto.LoginUserInfo;
 import com.papaswatch.psw.service.UserService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import static com.papaswatch.psw.config.Constant.USER.SESSION;
 
@@ -23,8 +24,8 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public boolean login(@RequestBody KeyValue<String, String> userInfo, HttpSession session) {
-        return userService.login(userInfo, session);
+    public Response<Boolean> login(@RequestBody KeyValue<String, String> userInfo, HttpSession session) {
+        return Response.ok(userService.login(userInfo, session));
     }
 
     @GetMapping("/me")
@@ -36,9 +37,17 @@ public class UserController {
     public void logout(HttpSession session) {
         userService.logout(session);
     }
-  
+
     @PostMapping("/signup")
     public Response<Boolean> signup(@RequestBody SignupReq signupReq) {
         return Response.ok(userService.signup(signupReq));
+    }
+
+    @PostMapping(value = "/seller/validate", consumes = {"multipart/form-data"})
+    public Response<Boolean> validateSeller(@RequestPart("sellerValidateReq") SellerValidateReq sellerValidateReq, @RequestPart("certificationFile") MultipartFile certificationFile) {
+        log.info("Validating seller: " + sellerValidateReq);
+        log.info("Certification file: " + certificationFile);
+
+        return Response.ok(true);
     }
 }
