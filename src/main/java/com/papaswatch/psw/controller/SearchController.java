b13d4a3529f554bc8ecc0280326da3c87bc8a0db
ssiky <yangss@eseict.com>
2025-03-24 23:12:45 +0900

    feat(search): searchTemplate 생성

---
+++ src/main/java/com/papaswatch/psw/controller/SearchController.java
@@ -0,0 +1,52 @@
+package com.papaswatch.psw.controller;
+
+import com.papaswatch.psw.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/search")
+public class SearchController {
+    private final SearchService searchService;
+
+    @GetMapping("/{productId}")
+    public void getProductById(@PathVariable long productId) {
+        searchService.getProductById(productId);
+    }
+
+    @GetMapping("/simple/list")
+    public void getProductList() {
+        searchService.getProductList();
+    }
+
+    @GetMapping("/discount")
+    public void getDiscountProductItemList() {
+        searchService.getDiscountProductItemList();
+    }
+
+    @GetMapping("/trend")
+    public void getSearchTrend() {
+        searchService.getSearchTrend();
+    }
+
+    @GetMapping("/qna")
+    public void getProductQNAList() {
+        searchService.getProductQNAList();
+    }
+
+    @GetMapping("/qna/{productId}")
+    public void getProductQNA(@PathVariable long productId) {
+        searchService.getProductQNA(productId);
+    }
+
+    @GetMapping("/qna/{productId}")
+    public void getReplyProductQNA(@PathVariable long productId) {
+        searchService.getReplyProductQNA(productId);
+    }
+}
---
+++ src/main/java/com/papaswatch/psw/service/SearchService.java
@@ -0,0 +1,32 @@
+package com.papaswatch.psw.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SearchService {
+    public void getProductById(long productId) {
+    }
+
+    public void getProductList() {
+    }
+
+    public void getDiscountProductItemList() {
+    }
+
+    public void getSearchTrend() {
+    }
+
+    public void getProductQNAList() {
+    }
+
+    public void getProductQNA(long productId) {
+    }
+
+    public void getReplyProductQNA(long productId) {
+
+    }
+}
